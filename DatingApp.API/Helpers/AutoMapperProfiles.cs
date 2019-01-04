681acfa5d534c5f13a9214f545c39cb535b63036
Guilherme Seiti Yamanaka <gseitiya@everis.com>
2019-01-04 13:03:50 -0200

    End of Section 8

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using DatingApp.API.DTOS;
+using DatingApp.API.Models;
+using System.Linq;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p =>
+                    p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d =>
+                    d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForDetailDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p =>
+                    p.IsMain).Url);
+                });
+            CreateMap<Photo, PhotosForDetailDto>();
+        }
+    }
+}
