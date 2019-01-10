bc5a75f816a79beced57d0679f53d7e3b3047725
Guilherme Seiti Yamanaka <gseitiya@everis.com>
2019-01-10 11:33:40 -0200

    End of Section 13

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -42,6 +42,7 @@ namespace DatingApp.API
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddTransient<Seed>();
+            services.AddScoped<LogUserActivity>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddJsonOptions(opt => {
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
